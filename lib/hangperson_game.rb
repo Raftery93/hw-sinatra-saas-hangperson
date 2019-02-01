7a6f39e0f38166bb86c7038c667d5efcde1f2328
EC2 Default User <ec2-user@ip-172-31-27-80.us-east-2.compute.internal>
2019-02-01 13:27:02 +0000

    issue 2 fixed

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -9,34 +9,37 @@ class HangpersonGame
   # end
   
   attr_accessor :word
-
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
   
   # Initialize the game
   def initialize(word)
     @word = word
     @guesses = ''
-    
-
+    @wrong_guesses = ''
+    @word_with_guesses = ''
+   
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
-  def self.get_random_word
+ def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
-    Net::HTTP.new('watchout4snakes.com').start { |http|
-      return http.post(uri, "").body
-    }
-    
-    
+    Net::HTTP.post_form(uri ,{}).body
   end
-  
+    
+    
 def guess(letter)
-   
-
+    letter.downcase!
+    
+    if word.include? letter
+     
+        return true
+      end
+    
   end
-  
-  
 end
